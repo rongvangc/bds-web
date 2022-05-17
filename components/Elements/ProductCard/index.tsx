82f8e06df168c829f2de43e443118c596de56cdf
“phamdaitai” <phamdaitai9899@gmail.com>
2022-05-17 18:07:31 +0700

    Resolve conflict

--- components/Elements/ProductCard/index.tsx
+++ components/Elements/ProductCard/index.tsx
@@ -1,67 +1,41 @@
 import React from 'react';
 import Image from 'next/image';
-import { LocationIcon, HomeIcon } from '../../../icons';
-import Badge from '../../../components/Badge';
-
-const imageUrl =
-  'https://file4.batdongsan.com.vn/crop/240x180/2022/05/11/20220511152802-1ac9.jpg';
+import { LocationIcon } from '../../../icons';
+import LinkButton from '../LinkButton';
 
 type ProductCardType = {};
 
 const ProductCard: React.FC<ProductCardType> = () => {
   return (
-    <div className="mb-4 flex rounded-md shadow">
-      <div className="product-preview-image">
+    <LinkButton href="/test">
+      <div className="rounded-md shadow">
         <Image
-          src={imageUrl || '/default-product-image.png'}
-          width={240}
-          height={180}
+          src="/default-product-image.png"
+          width={300}
+          height={200}
+          objectFit="cover"
           alt=""
         />
-        <div className="flex justify-between">
-          <Image
-            src={imageUrl || '/default-product-image.png'}
-            width={119}
-            height={66}
-            alt=""
-          />
-          <Image
-            src={imageUrl || '/default-product-image.png'}
-            width={119}
-            height={66}
-            alt=""
-          />
+        <div className="border-t-2 border-tertiary px-2 pt-1 pb-2">
+          <h3 className="text-md mb-2 font-semibold">Tên sản phẩm</h3>
+          <p className="mb-1 flex items-center text-sm">
+            <LocationIcon width={14} height={14} />
+            <span className="ml-1">1 tỷ500</span>
+            <span className="ml-4">200m2</span>
+          </p>
+          <p className="mb-1 flex items-center text-sm">
+            <LocationIcon width={14} height={14} />
+            <span className="ml-1">Lê Hồng Phong, Bmt</span>
+          </p>
+          <div className="flex items-center justify-between text-xs">
+            <p className="mb-0">20/06/2022</p>
+            <p className="mb-0 rounded-md bg-tertiary px-2 py-1 text-white">
+              Đã bán
+            </p>
+          </div>
         </div>
       </div>
-      <div className="product-item-content border-tertiary p-2">
-        <div className="mb-2">
-          <Badge text="Đã bán" />
-        </div>
-
-        <h3 className="text-md mb-2 font-semibold">
-          Mega Royal City Đồng Xoài
-        </h3>
-        <p className="mb-1 flex items-center text-sm">
-          <span className="font-semibold">10 triệu/m²</span>
-          <span className="ml-4	font-semibold">6.64 ha</span>
-          <span className="ml-4	font-semibold">525</span>
-          <HomeIcon width={14} height={14} />
-        </p>
-        <p className="mb-1 flex items-center text-sm">
-          <LocationIcon width={14} height={14} />
-          <span className="ml-1">
-            Phường Bình Thuận, Quận Hải Châu, Đà Nẵng.
-          </span>
-        </p>
-        <p className="truncate-2-line mb-0 text-sm font-medium">
-          Landmark Tower Đã Nẵng là dự án căn hộ cao cấp có vị trí đắc địa bên
-          bờ sông Hàn, TP. Đà Nẵng. Dự án do Công ty CP Cosmo Housing làm chủ
-          đầu tư, quy mô gồm 2 tòa tháp cao 31 và 39 tầng với hơn 400 căn hộ cao
-          cấp. Landmark Tower Đà Nẵng được kỳ vọng sẽ trở thành công trình biểu
-          tượng của Đà Nẵng.
-        </p>
-      </div>
-    </div>
+    </LinkButton>
   );
 };
 
